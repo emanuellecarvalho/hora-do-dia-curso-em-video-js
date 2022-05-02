043b616d35cc2a4484cb1539044b06a178805c56
Emanuelle Carvalho <79016261+emanuellecarvalho@users.noreply.github.com>
2022-05-02 11:27:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
-        img.src = '/images/manha.jpg'
+        img.src = 'images/manha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        img.src = '/images/tarde.jpg'
+        img.src = 'images/tarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        img.src = '/images/noite.jpg'
+        img.src = 'images/noite.jpg'
         document.body.style.background = '#515154'
     }
 }
